47d2dcaa865e6251a062b5147cac7190fbdfdaff
Wasyl Mowczan <movchanvasyl@gmail.com>
2020-06-22 20:32:18 +0200

    wprowadzenie lambda wyrazen zamiast komparatorow

--- src/pl/javastart/library/app/LibraryControl.java
+++ src/pl/javastart/library/app/LibraryControl.java
@@ -9,9 +9,7 @@ import pl.javastart.library.model.Book;
 import pl.javastart.library.model.Library;
 import pl.javastart.library.model.LibraryUser;
 import pl.javastart.library.model.Magazine;
-import pl.javastart.library.model.comparator.AlphabeticalTitleComparator;
 
-import java.util.Comparator;
 import java.util.InputMismatchException;
 
 public class LibraryControl {
@@ -129,20 +127,22 @@ public class LibraryControl {
     }
 
     private void printBooks() {
-        printer.printBooks(library.getSortedPublications(new AlphabeticalTitleComparator()));
+        printer.printBooks(library.getSortedPublications(
+                (p1, p2) -> p1.getTitle().compareToIgnoreCase(p2.getTitle())
+        ));
     }
 
     private void printMagazines() {
-        printer.printMagazines(library.getSortedPublications(new AlphabeticalTitleComparator()));
+        printer.printMagazines(library.getSortedPublications(
+                (p1, p2) -> p1.getTitle().compareToIgnoreCase(p2.getTitle())
+        ));
     }
 
     private void printUsers() {
-        printer.printUsers(library.getSortedUsers(new Comparator<LibraryUser>() {
-            @Override
-            public int compare(LibraryUser p1, LibraryUser p2) {
-                return p1.getLastName().compareToIgnoreCase(p2.getLastName());
-            }
-        }));
+        printer.printUsers(library.getSortedUsers(
+                (p1, p2) -> p1.getLastName().compareToIgnoreCase(p2.getLastName()
+                )
+        ));
     }
 
     private void deleteMagazine() {
